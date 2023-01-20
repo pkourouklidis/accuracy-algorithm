1c4b0bd8f9ad20fb5199e09d8372c570eba21f51
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2023-01-20 16:29:43 +0000

    initial commit

---
+++ detector.py
@@ -0,0 +1,10 @@
+from sklearn.metrics import accuracy_score
+
+#accuracy degradation detector
+def detector(trainSet, liveSet, parameters):
+    firstColumn = liveSet.axes[1][0]
+    secondColumn = liveSet.axes[1][1]
+    
+    accuracy = accuracy_score(liveSet[firstColumn].to_list(), liveSet[secondColumn].to_list())
+    threshold = float (parameters.get("threshold", 0.7))
+    return int(accuracy < threshold), accuracy
